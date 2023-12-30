abccb2fb50cff010fe5da9781f1a193119e7c876
wingo65 <nelkhaouli@email.neit.edu>
2023-12-30 14:06:25 -0500

    updated

--- RandS/script.js
+++ RandS/script.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
     });
   });
 
+
   document.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
@@ -25,4 +26,17 @@ $(document).ready(function(){
 
     const target = document.querySelector('.container-fluid2');
     observer.observe(target);
+});
+
+window.addEventListener('scroll', function() {
+  var images = document.querySelectorAll('.container-fluid3 img'); // Select all images
+  var screenPosition = window.innerHeight / 1.3;
+
+  images.forEach(function(image) {
+    var imagePosition = image.getBoundingClientRect().top;
+    
+    if (imagePosition < screenPosition) {
+      image.style.opacity = '1'; // Fade in the image
+    }
+  });
 });
