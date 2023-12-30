bf312a5fc6a36baffdd392215fd0b2606e6a8b66
wingo65 <nelkhaouli@email.neit.edu>
2023-12-30 17:18:01 -0500

    updated

--- RandS/script.js
+++ RandS/script.js
@@ -1,20 +1,3 @@
-$(document).ready(function(){
-    $('.navbar-nav .nav-link').click(function(event) {
-      // Check if the window width is less than or equal to 768px
-      if ($(window).width() <= 768) {
-        // Prevent the default action
-        event.preventDefault();
-        // Remove the active class from all links
-        $('.navbar-nav .nav-link').removeClass('active');
-      } else {
-        // For larger screens, toggle the active class as usual
-        $('.navbar-nav .nav-link').removeClass('active');
-        $(this).addClass('active');
-      }
-    });
-  });
-
-
   document.addEventListener('DOMContentLoaded', () => {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
